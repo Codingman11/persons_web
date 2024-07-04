4ca2cc7148cb991a1831051764f72714c15c7e26
JessePel <jesse1.peltola@outlook.com>
2024-07-05 01:08:41 +0300

    Update index.js

--- index.js
+++ index.js
@@ -30,20 +30,25 @@ let persons = [
     }
 ]
 
+//Creating a token for getting req.body POST
+morgan.token('body', req => {
+  return JSON.stringify(req.body)
+})
 
 
-
-const requestLogger = (request, response, next) => {
-  console.log('Method:', request.method)
-  console.log('Path:  ', request.path)
-  console.log('Body:  ', request.body)
-  console.log('---')
-  next()
-}
-
+app.use(morgan(function (tokens, req, res) {
+  return [
+    tokens.method(req, res),
+    tokens.url(req, res),
+    tokens.status(req, res),
+    tokens.res(req, res, 'content-length'), '-',
+    tokens['response-time'](req, res), 'ms',
+    tokens['body'](req)
+  ].join(' ')
+}))
 
 app.use(express.json())
-app.use(requestLogger)
+
 
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
@@ -64,24 +69,26 @@ const generateId = () => {
   return maxId + 1
 }
 
-app.post(baseUrl, (request, response) => {
-  const body = request.body
-
-  if (!body.name || !body.number) {
-    return response.status(400).json({ 
-      error: 'content missing' 
+app.post('/api/persons', (req, res) => {
+  const body = req.body
+  
+  if (!body.name || !body.number)  {
+    return res.status(400).json({
+      error: 'The name or number is missing'
+    })
+  } else if (persons.some(person => person.name === body.name)) {
+    return res.status(400).json({
+      error: 'The name already existing in the phonebook'
     })
   }
-
   const person = {
-    id: String(generateId()),
-    name: body.name,
-    number: body.number
+    "id": generateId(),
+    "name": body.name,
+    "number": body.number
   }
-
   persons = persons.concat(person)
-
-  response.json(person)
+  res.json(person)
+ 
 })
 
 app.put('/api/persons/:id', (req, res) => {
